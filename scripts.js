58ca147bca01f7293efa4b1449e70ac9506a09b7
Oliwia <97984851+CieniuchOliwia@users.noreply.github.com>
2022-03-09 19:36:48 +0100

    hover effect

--- scripts.js
+++ scripts.js
@@ -6,21 +6,22 @@ for(let i=1;i<=256;i++){
     let item= document.createElement('div');
     item.id='item'+i;
     item.classList.add('squares');
-    //item.textContent=i;
     container.appendChild(item);
+    item.addEventListener('mouseenter', function(){
+       item.style.backgroundColor='grey';
+    })
 }
 
 
 let containerStyle=document.querySelector('#container').style;
-//containerStyle.width='640px';
-//containerStyle.height='640px';
 containerStyle.display='grid';
 containerStyle.gridTemplateColumns='repeat(16, 40px)';
 containerStyle.gridTemplateRows='repeat(16, 40px)';
-//containerStyle.border='2px solid black';
 containerStyle.justifyContent='center';
 containerStyle.alignContent='center';
 document.querySelectorAll('.squares').forEach(el=>{el.style.border='1px solid grey'});
+
+
 
 
 for(let i=1;i<=16;i++){
@@ -38,4 +39,8 @@ for(let i=1;i<=241;i=i+16){
 for(let i=16;i<=256;i=i+16){
     document.querySelector('#item'+i).style.borderRight='2px solid grey';
 }
+
+
+
+
 
